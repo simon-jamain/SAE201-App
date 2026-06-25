aac2cbf7f498630cc779c49b8e6395527cab46f6
JAMAIN Simon <simon.jamain@etu.u-pec.fr>
2026-06-25 08:14:00 +0200

    Supression des restes non utilisés d'une ancienne version dans accueil.py

--- controllers/accueil.py
+++ controllers/accueil.py
@@ -1,6 +1,4 @@
 from flask import Blueprint, render_template
-from models.db import Session
-from models.dimensions import Region, ProfessionSante
 
 bp_accueil = Blueprint("accueil", __name__)
 
@@ -8,11 +6,5 @@ bp_accueil = Blueprint("accueil", __name__)
 @bp_accueil.route("/")
 @bp_accueil.route("/accueil")
 def index():
-    """Page d'accueil : formulaire de sélection."""
-    session = Session()
-    try:
-        regions     = session.query(Region).order_by(Region.libelle).all()
-        professions = session.query(ProfessionSante).order_by(ProfessionSante.libelle).all()
-        return render_template("accueil.html", regions=regions, professions=professions)
-    finally:
-        session.close()
+    """Page d'accueil."""
+    return render_template("accueil.html")
